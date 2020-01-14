72c1008ac8819fb46d82f79c24f0d11d1c4e790b
calm-block-7153 <rebeccahunt034@gmail.com>
2020-01-14 22:34:57 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,14 @@
 class Project
-  attr_accessor :backers, :backer
+  attr_accessor :backers, :add_backer
   attr_reader :title
    
   
   def initialize(title)
     @title = title
     @backers = []
+   end
+   
+   def add_backer(backer)
+     @backers << backer
    end
 end
